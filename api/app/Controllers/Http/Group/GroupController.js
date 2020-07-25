eb662183e09ad651572f8daa0e64f10c52901fab
StephenKuf <stephensleem@gmail.com>
2020-07-25 08:53:16 +0100

    fix

--- api/app/Controllers/Http/Group/GroupController.js
+++ api/app/Controllers/Http/Group/GroupController.js
@@ -4,31 +4,24 @@ const Group = use("App/Models/Group");
 const GroupUser = use("App/Models/GroupUser");
 
 class GroupController {
-  async createGroup({
-    request,
-    response,
-    auth
-  }) {
+  async createGroup({ request, response, auth }) {
     try {
-      const {
-        user
-      } = auth.current;
+      const { user } = auth.current;
 
-      const {
-        group_name,
-        group_bio,
-        profile_photo
-      } = request.all
+      const { group_name, group_bio, profile_photo } = request.all;
 
-      const groupCreate = await Group.findOrCreate({
-        group_name: group_name,
-        group_bio: group_bio,
-        profile_photo: profile_photo
-      }, {
-        group_name: group_name,
-        group_bio: group_bio,
-        profile_photo: profile_photo
-      })
+      const groupCreate = await Group.findOrCreate(
+        {
+          group_name: group_name,
+          group_bio: group_bio,
+          profile_photo: profile_photo
+        },
+        {
+          group_name: group_name,
+          group_bio: group_bio,
+          profile_photo: profile_photo
+        }
+      );
 
       if (!groupCreate) {
         return response.status(400).json({
@@ -37,6 +30,29 @@ class GroupController {
           message: `There was an error creating a group`
         });
       }
+      if (groupCreate) {
+        const logGroupAdmin = await GroupUser.findOrCreate(
+          {
+            user_id: user.id,
+            group_id: groupCreate.id,
+            is_admin: 1
+          },
+          {
+            user_id: user.id,
+            group_id: groupCreate.id,
+            is_admin: 1
+          }
+        );
+
+        if (!logGroupAdmin) {
+          return response.status(400).json({
+            label: `create Group Error`,
+            statusCode: 400,
+            message: `There was an error creating a group`
+          });
+        }
+      }
+
       response.status(200).json({
         label: "Group Creation",
         message: "Group Created Successfully",
@@ -48,8 +64,8 @@ class GroupController {
         error,
         label: `Group Creation`,
         statusCode: 400,
-        message: `Internal Server Error `,
-      })
+        message: `Internal Server Error `
+      });
     }
   }
 }
