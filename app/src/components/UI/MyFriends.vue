881507341db1d4807399614119d248c77ddd3926
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-08-28 08:05:32 +0100

    fix: Update app ui

--- app/src/components/UI/MyFriends.vue
+++ app/src/components/UI/MyFriends.vue
@@ -1,8 +1,20 @@
 <template>
   <section class="groups m t-3 bg-white p-3 my-3">
     <h3 class="f-16 f-bold">Friends</h3>
-
-    <div class="row mt-3" v-for="(each_friend, i) in friends" :key="i">
+    <div class="row my-3" v-if="!friends.length">
+      <div class="d-flex justify-content-center w-100">
+        <img
+          src="/assets/svg/add-friend.svg"
+          class="c-pointer"
+          alt="no friends"
+          @click="$router.push('/account/dashboard/belivers-network')"
+        />
+      </div>
+      <div class="text-center w-100 mt-2">
+        <span class="font-weight-bold">Please add a friend</span>
+      </div>
+    </div>
+    <div class="row my-3" v-for="(each_friend, i) in friends" :key="i" v-else>
       <div class=" col-lg-3">
         <img src="/assets/images/group-4.png" class="w-40" alt="user" />
       </div>
@@ -16,7 +28,7 @@
         <p class="c-brand f-14 f-med">Join</p>
       </div> -->
     </div>
-    <div class="text-center c-brand f-med mt-3 mb-3">
+    <div class="text-center c-brand f-med mt-3 mb-3" v-if="friends.length > 5">
       View all
     </div>
   </section>
@@ -33,4 +45,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.c-pointer {
+  cursor: pointer !important;
+}
+</style>
