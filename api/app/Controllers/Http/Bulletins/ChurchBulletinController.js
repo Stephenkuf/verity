d19f6cc5965cb3938f6ad06f2a14bb63b2e342cf
Stephen Kuforiji <stephenkuforiji@crenettechlabs.com>
2021-02-10 14:44:43 +0100

    view Bulletin for branch and customer foxed

--- api/app/Controllers/Http/Bulletins/ChurchBulletinController.js
+++ api/app/Controllers/Http/Bulletins/ChurchBulletinController.js
@@ -13,7 +13,7 @@ class ChurchBulletinController {
     async createChurchBulletin({ request, response, auth }) {
 
     try {
-      let userDenomination;
+      let userDenomination,LogchurchBulletin;
       const {bulletin_type, bulletin_subject, bulletin_body ,branch_id} = request.all();
         const {user} = auth.current;
 
@@ -25,27 +25,7 @@ class ChurchBulletinController {
         });
       }
     
-      if(bulletin_type == "denomination"){
-          userDenomination = await denominationInfo.query().where("user_id", user.id).first()
-          console.log("user denomination",userDenomination );
-            if (!userDenomination) {
-              return response.status(400).json({
-                label: `branch Information`,
-                statusCode: 400,
-                message: `Could not find user denomination.`,
-              });
-            }
-      } else {
-        const branchInformation = await branchInfo.findBy("id",branch_id )
-
-        if (!branchInformation) {
-          return response.status(400).json({
-            label: `Branch Information`,
-            statusCode: 400,
-            message: `There was an error fetching Branch Information`,
-          });
-        }
-      }
+      
           const churchBulletinCreation = await churchBulletin.create({
             bulletin_subject, 
             bulletin_body
@@ -59,19 +39,57 @@ class ChurchBulletinController {
             });
           }
 
-         const LogchurchBulletin= await churchBulletinLog.findOrCreate({
-              bulletin_id:churchBulletinCreation.id,
-              sender_id:user.id,
-              branch_id:branch_id,
-          })
+          if(bulletin_type == "denomination"){
+            userDenomination = await denominationInfo.query().where("user_id", user.id).first()
+            console.log("user denomination",userDenomination );
+              if (!userDenomination) {
+                return response.status(400).json({
+                  label: `branch Information`,
+                  statusCode: 400,
+                  message: `Could not find user denomination.`,
+                });
+              }
+            LogchurchBulletin= await churchBulletinLog.findOrCreate({
+                bulletin_id:churchBulletinCreation.id,
+                sender_id:user.id,
+                denomination_id:userDenomination.id
+            })
+  
+             if (!LogchurchBulletin) {
+              return response.status(400).json({
+                label: `Bulletin Creation `,
+                statusCode: 400,
+                message: `There was an error creating Bulletin.`,
+              });
+            }
 
-           if (!LogchurchBulletin) {
+        } else {
+          const branchInformation = await branchInfo.findBy("id",branch_id )
+  
+          if (!branchInformation) {
             return response.status(400).json({
-              label: `Bulletin Creation `,
+              label: `Branch Information`,
               statusCode: 400,
-              message: `There was an error creating Bulletin.`,
+              message: `There was an error fetching Branch Information`,
             });
           }
+
+          LogchurchBulletin= await churchBulletinLog.findOrCreate({
+            bulletin_id:churchBulletinCreation.id,
+            sender_id:user.id,
+            branch_id:branch.id
+        })
+
+         if (!LogchurchBulletin) {
+          return response.status(400).json({
+            label: `Bulletin Creation `,
+            statusCode: 400,
+            message: `There was an error creating Bulletin.`,
+          });
+        }
+        
+        }
+        
 
           response.status(200).json({
             label: "Bulletin Creation",
@@ -92,43 +110,46 @@ class ChurchBulletinController {
     //  view Requests sent 
     async viewChurchBulletin({request , response , auth}){
         try {
-          let is_branch, churchBulletin;
+          let is_branch, churchBulletin,userDetails;
          const {user} = auth.current;
         //  get userchurch and denomination first 
         // check if its a denomination 
         const is_denomination = await denominationInfo.findBy("user_id", user.id);
-        
-        churchBulletin = await Database
-        .table('bulletin_users')
-        .leftJoin('church_bulletins', 'bulletin_id', 'church_bulletins.id')
-        .where("sender_id", user.id).andWhere('is_deleted', 0)
-      
-
-         if (!is_denomination){
-          is_branch = await branchInfo.findBy("user_id", user.id);
-
+        if (is_denomination) {
           churchBulletin = await Database
           .table('bulletin_users')
-          .leftJoin('church_bulletin', 'bulletin_id', 'church_bulletin.id')
-          .where(function () {
-            this.where("branch_id", is_branch.id).orWhere("denomination_id", is_branch.denomination_id)
-          }) 
-          .andWhere(function () {
-            this.where('is_deleted', 0)
-          })
+          .leftJoin('church_bulletins', 'bulletin_id', 'church_bulletins.id')
+          .where("sender_id", user.id).andWhere('is_deleted', 0)
+        }
+        
+      
+         if (!is_denomination || is_denomination == null){
+          is_branch = await branchInfo.findBy("user_id", user.id);
+
+          if(is_branch ||is_branch != null ){
+            churchBulletin = await Database
+            .table('bulletin_users')
+            .leftJoin('church_bulletins', 'bulletin_id', 'church_bulletins.id')
+            .where(function () {
+              this.where("branch_id", is_branch.id).orWhere("denomination_id", is_branch.denomination_id)
+            }) 
+            .andWhere(function () {
+              this.where('is_deleted', 0)
+            })
+          }
+
            if (!is_branch){
              userDetails = await additionalUserInfo.query().where("user_id", user.id).first()
 
-             await Database
+             churchBulletin =  await Database
               .table('bulletin_users')
-              .leftJoin('church_bulletin', 'bulletin_id', 'church_bulletin.id')
+              .leftJoin('church_bulletins', 'bulletin_id', 'church_bulletins.id')
               .where(function () {
                 this.where("branch_id", userDetails.branch_id).orWhere("denomination_id", userDetails.branch_id)
               }) 
               .andWhere(function () {
                 this.where('is_deleted', 0)
               })
-            
            }
          }
            
